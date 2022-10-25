6d8964d575cefcd6b4168fad0553113fee133ae2
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2022-10-25 23:58:50 +0200

    Add files via upload

---
+++ Урок 13 IF ELSE/Урок 13 IF ELSE/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Конструкция if else
+ * 
+  */
+
+
+
+namespace Урок_13_IF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+         bool isInfected = false;
+
+            if (isInfected)
+            {
+                Console.WriteLine("Персонаж инфицирован!");
+            }
+            else
+            {
+                Console.WriteLine("Персонаж здоров!");
+               
+            } */
+
+
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            if ( a == 5)
+            {
+                Console.WriteLine("а равно 5");
+            }
+
+            else
+            {
+                Console.WriteLine("a не равно 5");
+            }
+
+        }
+    }
+}
---
+++ Урок 13 IF ELSE/Урок 13 IF ELSE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Урок 13 IF ELSE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Урок 13 IF ELSE")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7f4e0644-94a7-4378-a810-75addb0e2472")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
