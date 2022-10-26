a4e488ed75abbe71ea35670b1af7ef3d0e653e7a
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2022-10-26 07:49:17 +0200

    Add files via upload

---
+++ методы начало/методы начало/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace методы_начало
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void SayHello () { Console.WriteLine("Hello"); }
+            void SayHello1() => Console.WriteLine("Hello1");
+
+            SayHello();
+            SayHello1();
+
+        }
+    }
+}
