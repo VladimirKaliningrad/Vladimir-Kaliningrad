f8c384582ec768902e57d1e7810bab39ba027236
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2022-11-03 07:29:49 +0200

    Add files via upload

---
+++ метод, функция1 CS/метод, функция1 CS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace метод__функция1_CS
+{
+    internal class Program
+    {
+        static void PrintLine (string symbol, uint symbolsCount)
+        {
+            for (int i = 0; i < symbolsCount; i++)
+                Console.WriteLine(symbol);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            string symbol = Console.ReadLine();
+
+            Console.WriteLine("Введите количество символов ");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+            PrintLine(symbol, symbolsCount);
+            Console.ReadLine();
+
+        }
+    }
+}
