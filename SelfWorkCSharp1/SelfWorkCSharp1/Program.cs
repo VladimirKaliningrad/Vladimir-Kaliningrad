7a7d948f0a36034f4fe6fc22c4a6afa78e0d9623
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-01-08 22:44:55 +0200

    Add files via upload

---
+++ SelfWorkCSharp1/SelfWorkCSharp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SelfWorkCSarp
+{
+    /* треугольник
+     * ключевое слово continue
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                 Console.WriteLine();
+            }
+            
+          
+            /*for (int i=0; i < 5;i++)
+            {
+                if (i==3)
+                {
+                    continue;
+                }
+                if (i==1)
+                {
+                    continue;
+                }
+
+
+                Console.WriteLine(i);
+
+            }
+            Console.ReadLine();*/
+        }
+    }
+
+
+}
