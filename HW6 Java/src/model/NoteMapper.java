c3cc4f89bc8d5fd765aee3a77aaa12c3afc4799b
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-01-23 15:13:26 +0200

    Delete HW6 Java directory

--- HW6 Java/src/model/NoteMapper.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class NoteMapper {
-    public Note map(String line) {
-        String[] parts = line.split(" / ");
-        return new Note(parts[0], parts[1], parts[2], parts[3]);
-    }
-}
