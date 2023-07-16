2ad63e453806e34eded92b8f640d0f405558026d
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-07-16 21:56:09 +0200

    Add files via upload

---
+++ Lessons_2_JS/script.js
@@ -0,0 +1,55 @@
+// Задание 1
+console.log('Текст из script.js!');
+// Задание 2
+//////////////////////////////////////////////////
+let num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+
+console.log(a);
+
+let b = 1 + 2 + 3;
+
+console.log(b);
+
+// комментарии
+
+/*комментарии
+*/
+/////////////////////////////////////////////////
+const c =10;
+const d =2;
+
+let sum = c + d;
+let sub = c - d;
+let mult = c * d;
+let div = c / d;
+
+console.log(c, d, sum, sub, mult, div);
+console.log(c + d + sum + sub + mult + div);
+
+/////////////////////////////////////////////////
+const e = 1.5;
+const f = 0.75;
+let summ = e + f;
+console.log(e + f);
+console.log(summ);
+
+let j = -5;
+console.log(-j);
+/////////////////////////////////////////////////
+let myName = {
+    firstName: "Vladimir",
+    lastName: "Ryzhenko",
+};
+console.log(`имя ${myName.firstName} фамилия ${myName.lastName}`);
+
+let java = "Jav";
+let script = "Script";
+console.log(java + script);
+
+let hello = "Hello";
+let world = "World";
+console.log(hello + " " + world);
