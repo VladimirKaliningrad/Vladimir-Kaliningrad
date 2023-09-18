c972b170e75db0f483f34922141de89c251d7aea
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-09-18 22:29:21 +0200

    Add files via upload

---
+++ unit-tests-1(Код семинара)/src/seminars/first/Shop/Product.java
@@ -0,0 +1,23 @@
+package seminars.first.Shop;
+
+public class Product {
+    private Integer cost; // Стоимость продукта
+    private String title; // Название
+
+    // Геттеры, сеттеры:
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
