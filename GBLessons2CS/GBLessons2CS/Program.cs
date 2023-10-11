595c4b82a8f16b1b34ccb1d0abe5b1030291c930
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-10-11 13:12:47 +0200

    Add files via upload

---
+++ GBLessons2CS/GBLessons2CS/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    //class SomeClass
+    //{
+    //    public int SomeVariable;
+    //}
+
+    //////////////таблица умножения в цыкле////////////////////////
+    static void Main(string[] args)
+    {
+        for (int i =0; i<=9; i++)
+        {
+            for (int k =0; k<=9; k++)
+            {
+                Console.Write($"{i}X{k}={i * k}".PadLeft(10));
+            }
+            Console.WriteLine();
+        }
+        //SomeClass a = new SomeClass();
+        //a.SomeVariable = 10;
+
+        //SomeClass b = a;
+        //a.SomeVariable = 20;
+
+        //Console.WriteLine($"a={a.SomeVariable}, b = {b.SomeVariable}");
+    }
+}
