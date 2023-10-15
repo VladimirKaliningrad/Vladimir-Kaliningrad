e13e98a9b112842fa3799f22d752cde66bc00eae
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-10-15 23:11:44 +0200

    Add files via upload

---
+++ GB_HW_CS1/FamilyMember.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_HW_CS1
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
+    public class FamilyMember
+    {
+        //public FamilyMember[] Parents { get; set; }
+        public FamilyMember Mother { get; set; }
+        public FamilyMember Father { get; set; }
+        public string Name { get; set; }
+        public Gender Sex { get; set; }
+
+        void Info(int indent = 0)
+        {
+            Console.WriteLine($"{new String('-', indent)}Имя {this.Name}");
+        }
+        public virtual void Print(int indent = 0)
+        {
+            Info(indent);
+        }
+
+
+    }
+
+    public class AdultFamilyMember : FamilyMember
+    {
+        public FamilyMember[] Children { get; set; }
+        public override void Print(int indent = 0)
+        {
+            base.Print(indent);
+
+            foreach (var child in Children)
+                child.Print(indent * 2);
+        }
+
+    }
+
+}
+
---
+++ GB_HW_CS1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GB_HW_CS1
+{
+    internal class Program
+    {//////Спроектируйте программу для построения генеалогического
+     //////дерева. Учтите что у нас есть члены семьи у кого нет
+        ///детей(дет). Есть члены семьи у кого дети есть (взрослые).
+        //////Есть мужчины и женщины.
+        static void Main(string[] args)
+        {
+            var grad_father = new AdultFamilyMember() { Mother = null, Name = "Dukki", Sex = Gender.Male };
+            var father = new AdultFamilyMember() { Mother = null, Name = "Папа", Sex = Gender.Male };
+            var mother =new AdultFamilyMember() { Mother = null, Father = null, Name = "Мама", Sex = Gender.Female};
+            var son = new FamilyMember() { Mother = mother, Father = mother, Name = " Сын", Sex = Gender.Male };
+            mother.Children = new FamilyMember[] { son };
+            father.Children = new FamilyMember[] { son };
+
+
+
+            grad_father.Print();
+            Console.ReadLine();
+        }
+    }
+}
