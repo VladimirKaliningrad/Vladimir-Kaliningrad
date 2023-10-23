2b5b2f8323ec9e0ea04f21c1148c18fc143076f6
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-10-23 21:33:35 +0200

    Add files via upload

---
+++ GB_HW_CS2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_HW_CS2
+
+{
+    public interface IBits
+    {
+        bool GetBit(int i);
+        void SetBit(bool bit, int index);
+    }
+
+
+    public class Bits : IBits
+    {
+        public Bits(byte b)
+        {
+            this.Value = b;
+            MaxBitsCount = sizeof(byte) * 8;
+        }
+
+        public Bits(short b)
+        {
+            this.Value = b;
+            MaxBitsCount = sizeof(short) * 8;
+        }
+
+        public Bits(int b)
+        {
+            this.Value = b;
+            MaxBitsCount = sizeof(int) * 8;
+        }
+
+        public Bits(long b)
+        {
+            this.Value = b;
+            MaxBitsCount = sizeof(long) * 8;
+        }
+
+        public long Value { get; set; } = 0;
+        private int MaxBitsCount { get; set; }
+
+
+
+        public bool GetBit(int index)
+        {
+            if (index > MaxBitsCount || index < 0)
+            {
+                Console.WriteLine($"Выход за пределы от 0 до {MaxBitsCount}");
+                return false;
+            }
+
+            return ((Value >> index) & 1) == 1;
+        }
+
+        public void SetBit(bool bit, int index)
+        {
+            if (index > MaxBitsCount || index < 0)
+            {
+                Console.WriteLine($"Выход за пределы от 0 до {MaxBitsCount}");
+                return;
+            }
+            if (bit == true)
+                Value = (byte)(Value | (1 << index));
+            else
+            {
+                var mask = (byte)(1 << index);
+                mask = (byte)(0xff ^ mask);
+                Value &= (byte)(Value & mask);
+            }
+        }
+    }
+
+
+
+}
