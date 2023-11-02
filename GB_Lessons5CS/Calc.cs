aef244254197854c95a960e44ad1e89dcc7513ad
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-02 22:14:02 +0200

    Add files via upload

---
+++ GB_Lessons5CS/Calc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Lessons5CS
+{
+    internal class Calc : ICalc
+    {
+        public double Result { get; set; } = 0;
+
+        private Stack<double> LastResult { get; set; } = new Stack<double>();
+
+        public event EventHandler<EventArgs> MyEventHandler;
+
+        private void PrintResult()
+        {
+            MyEventHandler?.Invoke(this, new EventArgs());
+        }
+
+        public void Divide(int x)
+        {
+            Result /= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Multy(int x)
+        {
+            Result *= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sub(int x)
+        {
+            Result -= x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void Sum(int x)
+        {
+            Result += x;
+            PrintResult();
+            LastResult.Push(Result);
+        }
+
+        public void CancelLast()
+        {
+            if (LastResult.TryPop(out double res))
+            {
+              Result = res;
+
+              Console.WriteLine("Последнее действие отменено. Результат равен: ");
+              PrintResult();
+            }
+            else
+            {
+                Console.WriteLine("Невозможно отменить последнее действие");
+            }
+
+            
+        }
+
+    }
+}
+
+/*Арифметические методы должны выполняться как обычно
+    а метод CancelLast должен отменять последнее действие. 
+    При этом метод может отменить последовательно все действия 
+    вплоть до самого последнего. Спросите как студенты планируют 
+    реализовать отмену действия и если будут 
+    трудности с ответами то подскажите им про стек*/
---
+++ GB_Lessons5CS/ICalc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Lessons5CS
+{
+    public interface ICalc
+    {
+        double Result { get; set; }
+        void Sum(int x);
+        void Sub(int x);
+        void Multy(int x);
+        void Divide(int x);
+        void CancelLast();
+        event EventHandler<EventArgs> MyEventHandler;
+    }
+}
