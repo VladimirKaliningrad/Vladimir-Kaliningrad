eb13012f4c1166a006fc3b47fa6b3f3ad6988e94
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-17 17:34:05 +0200

    Add files via upload

---
+++ GBLessons6CS/NegativeNumberException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBLessons6CS
+{
+    public class NegativeNumberException : Exception
+    {
+        public NegativeNumberException()
+        {
+
+        }
+
+        public NegativeNumberException(string massege) : base(massege)
+        {
+
+
+        }
+
+        public NegativeNumberException (string massege, Exception ex) : base (massege, ex)
+        {
+
+
+        }
+
+
+    }
+}
---
+++ GBLessons6CS/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace GBLessons6CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (MyTryParse("string", out int num))
+            //{
+            //     Console.WriteLine(num);
+            //}
+            //////////////////////////////////////////////////////////////////////
+
+
+            try
+            {
+                ProcessNumber(-4);
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+            catch { }
+
+
+        }
+        /// 
+        /// //////////////////////////////////////////////////////////////////////////////
+        /// </summary>
+        /// <param name="number"></param>
+        /// <exception cref="NegativeNumberException"></exception>
+        //1 исключение если нет цифры то ошибка-исключение
+
+        //static bool MyTryParse(string myString, out int num)
+        //{
+        //    num = 0;
+        //    try
+        //    {
+        //        num = Convert.ToInt32(myString);
+        //    }
+        //    catch(FormatException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return false;
+        //    }
+        //    return true;
+        //}
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        static void ProcessNumber(int number)
+        {
+            if (number < 0)
+            {
+                throw new NegativeNumberException("ваше число отрицательное", new Exception ("ошибка исключение") );
+            }
+
+            Console.WriteLine(number);
+
+        }
+
+
+
+
+
+    }
+
+}
