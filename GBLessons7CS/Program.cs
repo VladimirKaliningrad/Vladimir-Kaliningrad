755907601c9d82dc8571d4988c5dbece883ccf3d
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-22 17:54:54 +0200

    Add files via upload

---
+++ GBLessons7CS/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace GBLessons7CS
+{
+    internal class MyClass
+    {
+        static int MyField = 3;
+        public int MyProperty => MyField * -1;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = Activator.CreateInstance(typeof(int[]), new object[] { 10 });
+
+            Console.WriteLine((obj as int[])?.Length);
+
+
+        }
+    }
+}
