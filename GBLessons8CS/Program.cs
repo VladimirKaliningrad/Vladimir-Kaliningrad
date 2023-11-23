3bbe6aadbdcdf5f142d782ea85a20f895e5c4176
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-23 20:41:06 +0200

    Add files via upload

--- GBLessons8CS/Program.cs
+++ GBLessons8CS/Program.cs
@@ -4,19 +4,30 @@
     {
         static void Main(string[] args)
         {
-            var bytes = new byte[101];
+            var d = Directory.GetLastAccessTime("bin/Debug");
+            Console.WriteLine(d);
 
-            for (int i = 0; i < 101; i++)
-                bytes[i] = (byte)i;
+            Directory.SetLastAccessTime("bin/Debug", d.AddDays(-10));
+            d = Directory.GetLastAccessTime("bin/Debug");
+            Console.WriteLine(d);
 
-            using (var stream = new MemoryStream(bytes))
-            {
-                int b = 0;
-                while ((b = stream.ReadByte()) >= 0)
-                {
-                    Console.Write((byte)b + " ");
-                }
-            } 
+            
+            
+            
+            
+            //var bytes = new byte[101];
+
+            //for (int i = 0; i < 101; i++)
+            //    bytes[i] = (byte)i;
+
+            //using (var stream = new MemoryStream(bytes))
+            //{
+            //    int b = 0;
+            //    while ((b = stream.ReadByte()) >= 0)
+            //    {
+            //        Console.Write((byte)b + " ");
+            //    }
+            //} 
      
 
         }
