c593f0afe1193bd5f1e08e62be6cddcd9c8f74e2
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-23 20:08:18 +0200

    Add files via upload

---
+++ GBLessons8CS/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace GBLessons8CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = new byte[101];
+
+            for (int i = 0; i < 101; i++)
+                bytes[i] = (byte)i;
+
+            using (var stream = new MemoryStream(bytes))
+            {
+                int b = 0;
+                while ((b = stream.ReadByte()) >= 0)
+                {
+                    Console.Write((byte)b + " ");
+                }
+            } 
+     
+
+        }
+    }
+}
