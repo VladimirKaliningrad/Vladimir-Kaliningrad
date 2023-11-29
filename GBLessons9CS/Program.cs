c0f1c26de5382248318edfe8e3c2b826271b38b4
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-11-29 14:55:46 +0200

    Add files via upload

---
+++ GBLessons9CS/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace GBLessons9CS
+{
+    public class Program
+    {
+        //public class Person
+        //{
+        //    public string Name;
+        //    public string Surname;
+        //    public int Age;
+        //    public object[] Details;
+        //}
+        //public class Address
+        //{
+        //    public string Street;
+        //    public int BuildingNo;
+        //}
+        //public class Phone
+        //{
+        //    public string Home;
+        //    public string Work;
+        //}
+
+        //public class Occupation
+        //{
+        //    public string Company;
+        //    public string Position;
+        //}
+
+        static void Main(string[] args)
+        {
+
+            ;
+            //var obj = new Person()
+            //{
+            //    Name = "Vladimir",
+            //    Surname = "Ryzhenko",
+            //    Age = 40,
+            //    Details = new object[]
+            //    {
+            //        new Address {Street = "Mashino", BuildingNo = 7},
+            //        new Phone {Home = "88121234567", Work ="881276554321"},
+            //        new Occupation {Company = "MVD", Position = "boss"}
+            //    }
+            //};
+
+            //Type[] types = new Type[] { typeof(Address), typeof(Phone), typeof(Occupation) };
+            //var serializer = new XmlSerializer(typeof(Person), types);
+
+            //serializer.Serialize(Console.Out, obj);
+
+
+
+
+        }
+    }
+}
