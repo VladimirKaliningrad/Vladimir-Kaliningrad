7be9320ecce1c09aa55b739a34a0075a19e05b21
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-12-03 23:23:56 +0200

    Add files via upload

---
+++ GB_HW_Connect1/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GB_HW_Connect1
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        
+        public DateTime DateTime { get; set; }
+
+        public string NicknameFrom { get; set; }
+
+        public string NicknameTo { get; set; }
+
+        public string SerializeMessageToJson() => JsonSerializer.Serialize(this);
+  
+        public static Message? DeserializeFromJson(string message) => JsonSerializer.Deserialize<Message>(message);
+
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{this.DateTime} получено сообщение {this.Text} от  {this.NicknameFrom}";
+        }
+
+
+    }
+
+    
+}
