10413a129bd2dbc6bc52b91e2b42296927976c61
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-12-04 17:45:57 +0200

    Add files via upload

---
+++ GBLessConnect2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GBLessConnect2
+{
+    class Program
+    {
+        static void PrintTheread()
+        {
+            for (int i = 0, i < 10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - one");
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - two");
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}-  three");
+
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i =0, i < 10; i++)
+            {
+                Thread t = new Thread(PrintTheread);
+                t.Start();
+            }
+        }
+    }
+}
