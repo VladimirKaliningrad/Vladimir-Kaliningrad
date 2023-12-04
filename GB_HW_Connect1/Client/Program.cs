56159f2aa603291c04773a64a136a15425b38678
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2023-12-04 07:25:36 +0200

    Add files via upload

---
+++ GB_HW_Connect1/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using GB_HW_Connect1;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SentMessage(args[0], args[1]);
+        }
+        public static void SentMessage(string From, string ip)
+        {
+
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
+
+
+            while (true)
+            {
+                string messageText;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите сообщение.");
+                    messageText = Console.ReadLine();
+
+                }
+                while (string.IsNullOrEmpty(messageText));
+
+                Message message = new Message() { Text = messageText, NicknameFrom = From, NicknameTo = "Server", DateTime = DateTime.Now };
+                string json = message.SerializeMessageToJson();
+
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                udpClient.Send(data, data.Length, iPEndPoint);
+            }
+
+        }
+    }
+}
