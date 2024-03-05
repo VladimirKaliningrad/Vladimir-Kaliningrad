18c99eacf28e19dfda41445cc19ecc432727be84
VladimirKaliningrad <110151622+VladimirKaliningrad@users.noreply.github.com>
2024-03-05 16:42:34 +0200

    Add files via upload

---
+++ CSharp-course-Lesson-and-DZ-8-main/FileSearchUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_course_Lesson_and_DZ_8
+{
+    internal class FileSearchUtility
+    {
+        public static void SearchForFile(string directory, string fileName)
+        {
+            var files = Directory.EnumerateFiles(directory, fileName, searchOption: SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Искомый файл {fileName} находится по пути: {file}");
+            }
+        }
+
+        public static void SearchForValueInFile(string fileName, string searchValue)
+        {
+            if (File.Exists(fileName))
+            {
+                using (StreamReader sr = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        if (line.Contains(searchValue))
+                        {
+                            Console.WriteLine($"Найдено значение {searchValue} в файле {fileName}: {line}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не существует");
+            }
+        }
+    }
+}
---
+++ CSharp-course-Lesson-and-DZ-8-main/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharp_course_Lesson_and_DZ_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = "путь_к_директории"; 
+            string fileName = "искомое_имя_файла"; 
+            string searchValue = "3333";
+
+            FileSearchUtility.SearchForFile(directory, fileName);
+            FileSearchUtility.SearchForValueInFile(fileName, searchValue);
+        }
+
+        
+
+
+
+    }
+}
